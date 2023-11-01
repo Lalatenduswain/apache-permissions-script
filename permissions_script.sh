8762c8049014a92ab583510e02b098f4747ff43b
Lalatendu Keshari Swain <lkswain_2003@yahoo.com>
2023-11-01 17:47:35 +0530

    Update permissions_script.sh

--- permissions_script.sh
+++ permissions_script.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Set ownership and permissions for directories
+# Set the web user variable
+if id -u www-data &>/dev/null; then
+    web_user="www-data"
+elif id -u apache &>/dev/null; then
+    web_user="apache"
+else
+    echo "Web user 'www-data' or 'apache' not found."
+    exit 1
+fi
+
+# Set ownership and permissions for directories and files
+find /var/www/html/ -type d -execdir chown $web_user:$web_user {} \; -execdir chmod 750 {} \;
+find /var/www/html/ -type f -execdir chown $web_user:$web_user {} \; -execdir chmod 640 {} \;
+
+# Set the setgid bit on the directory
 sudo chmod g+s /var/www/html/
-
-find /var/www/html/ -type d -execdir chown apache:apache {} \; -execdir chmod 750 {} \;
-
-# Set ownership and permissions for files
-find /var/www/html/ -type f -execdir chown apache:apache {} \; -execdir chmod 640 {} \;
-
-
-#Single Line Command
-#find /var/www/html/ \( -type d -execdir chown apache:apache {} \; -execdir chmod 750 {} \; \) -o \( -type f -execdir chown apache:apache {} \; -execdir chmod 640 {} \; \)
